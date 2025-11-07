d64bbae140708b0f9843c67bcb95ff1dd442db24
Brandom <brandom.quispe@gmail.com>
2025-11-07 20:05:25 -0300

    criando a função de criar a tabela

--- back-end/conexao.py
+++ back-end/conexao.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Conexão com o banco
-def conexao():
+def conectar_banco():
     try:
         conexao = psycopg2.connect(
             host=os.getenv("DB_HOST"),
@@ -16,6 +16,34 @@ def conexao():
             port=os.getenv("DB_PORT")
         )
         print("Conexão estabelecida com sucesso!")
+        return conexao
     except Exception as erro:
         print(f"A conexão falhou: {erro}")
+        return None
 
+#Função de criar a tabela 
+def criar_tabela():
+    conexao = conectar_banco()
+    if conexao:
+        try:
+            cursor = conexao.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS produtos(
+                                id SERIAL PRIMARY KEY,
+                                nome TEXT NOT NULL,
+                                categoria TEXT NOT NULL,
+                                preco REAL NOT NULL,
+                                quantidade INTEGER
+                           )""")
+            conexao.commit()
+            print("Tabela criada com sucesso!")
+            cursor.close()
+        except Exception as erro:
+            print(f"Erro ao criar a tabela:{erro}")
+        finally:
+            conexao.close()
+            print("Conexão fechada")
+    else:
+        print("Não foi possivel criar a tabela porque a conexão falhou.")
+
+if __name__ == "__main__":
+    criar_tabela()
